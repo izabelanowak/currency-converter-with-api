65552dbf2cfc67e3ac9ed712ebe98d0582b3984e
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-07-04 13:21:33 +0200

    currencies deleted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import Container from "./Container";
 import Form from "./Form";
-import currencies from "./currencies";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import { useAPI } from "./useAPI";
 import { ratesData } from "./data.js";
 
 function App() {
-  // const rates = JSON.parse("data.json");
-  // console.log(rates);
   useAPI();
 
   return (
     <ThemeProvider theme={theme}>
       <Container>
-        <Form currencies={currencies} rates={ratesData.rates} />
+        <Form rates={ratesData.rates} />
       </Container>
     </ThemeProvider>
   );
--- src/currencies.js
+++
@@ -1,8 +0,0 @@
-const currencies = [
-    { id: "EUR", exchangeRate: 4.5654, name: "Euro", },
-    { id: "GBP", exchangeRate: 5.2491, name: "Funt brytyjski", },
-    { id: "USD", exchangeRate: 3.7746, name: "Dolar amerykański", },
-    { id: "CHF", exchangeRate: 4.1502, name: "Frank szwajcarski", },
-];
-
-export default currencies;
